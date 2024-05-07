5c6997eeba7b6f90db8d2fe2dd45c7ab44dd4b04
lgsmith <louis.smith88@gmail.com>
2024-05-07 00:26:59 -0400

    Added update to use nagl if available. should be very helpful.

--- parameterize_system.py
+++ parameterize_system.py
@@ -1,5 +1,13 @@
 import argparse as ap
 from openff.toolkit import Molecule, Topology
+try:
+    from openff.toolkit.utils.nagl_wrapper import NAGLToolkitWrapper
+    from openff.nagl_models import get_models_by_type
+    nagl_model_path = get_models_by_type("am1bcc")[0]
+except ModuleNotFoundError:
+    nagl_model_path = None
+    print('Could not find NAGLToolkitWrapper; is openff-nagl installed?',
+          'Falling back to single point AM1BCC for charges.')
 from openmmforcefields.generators import SMIRNOFFTemplateGenerator
 from openmm.app import ForceField
 from openmm import XmlSerializer, CustomExternalForce
@@ -8,11 +16,13 @@ from pathlib import Path
 from math import sqrt
 
 
-def off_serialize(outdir: Path, name, openff_obj):
+def off_serialize(outdir: Path, name, openff_obj, sdf=False):
     json_str = openff_obj.to_json()
     outp = (outdir/name).with_suffix('.json')
     outp.write_text(json_str)
     openff_obj.to_file(outp.with_suffix('.pdb'))
+    if sdf:
+        openff_obj.to_file(outp.with_suffix('.sdf'))
     return outp
 
 
@@ -26,10 +36,12 @@ def omm_serialize(outdir: Path, name, omm_obj):
 p = ap.ArgumentParser(formatter_class=ap.ArgumentDefaultsHelpFormatter)
 p.add_argument('receptor_pdb', type=Path, 
                help='Receptor PDB file to parameterize.')
-p.add_argument('ligand_smiles', type=str,
-               help='Smiles string of ligand to parameterize.')
+p.add_argument('ligand', type=str,
+               help='SDF, or Smiles string of ligand to parameterize.')
 p.add_argument('out_dir', type=Path,
                help='Name of directory to write parameterized jsons and systems to.')
+p.add_argument('--ligand-sdf', '-s', action=ap.BooleanOptionalAction, default=False,
+               help='If thrown, interpret ligand as path to SDF file.')
 p.add_argument('--ligand-ff', type=str, default='openff-2.1.0-unconstrained.offxml',
                help='Name of force field to use as an argument to SMIRNOFFTemplateGenerator.')
 p.add_argument('--receptor-ff', type=str, default='amber/protein.ff14SB.xml',
@@ -40,6 +52,8 @@ p.add_argument('--implicit', type=str, default='gbn2.xml',
 p.add_argument('--restraint-k', type=float, default=None,
                help='Restrain receptor to the starting conformation with the provided force constant;'
                 ' for minimized energy evaluations. Assumes k is in  kcal/(mol * Angstrom).')
+p.add_argument('--write-sdf', action=ap.BooleanOptionalAction, default=True,
+               help='If cancelled don')
 
 args = p.parse_args()
 
@@ -52,17 +66,27 @@ kappa = 367.434915*sqrt(conc/(solv_dielectric*temperature))
 if not args.out_dir.is_dir():
     args.out_dir.mkdir(parents=True)
 
-ligand = Molecule.from_smiles(args.ligand_smiles)
-ligand.generate_conformers(n_conformers=10)
-print('Generated ligand conformers from SMILES. Prepping charges.', flush=True)
-ligand.assign_partial_charges(partial_charge_method='am1bcc', 
-                              use_conformers=ligand.conformers)
+if args.ligand_sdf:
+    ligand = Molecule.from_file(args.ligand, 'sdf')
+else:
+    ligand = Molecule.from_smiles(args.ligand)
+if nagl_model_path:
+    ligand.assign_partial_charges(nagl_model_path, toolkit_registry=NAGLToolkitWrapper())
+else:
+    ligand.generate_conformers(n_conformers=1)
+    print('Generated ligand conformers from SMILES. Prepping charges.', flush=True)
+    ligand.assign_partial_charges(partial_charge_method='am1bcc', 
+                                use_conformers=ligand.conformers)
 print('Finished getting ligand charges.', flush=True)
 receptor = Topology.from_pdb(args.receptor_pdb)
 print('Got receptor topology.')
 receptor_count = len(list(receptor.atoms))
 # make topologies, and serialize them
 lig_top = ligand.to_topology()
+if args.write_sdf:
+    # atom order not preserved by Molecule.to_topology().
+    top_ligand = Molecule.from_topology(lig_top)
+    top_ligand.to_file(args.out_dir.with_suffix('.sdf'), 'sdf')
 # always do this receptor first!
 rl_complex = receptor + lig_top
 # rl_complex = lig_top + receptor 
